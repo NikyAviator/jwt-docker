b78b6f2a37aef910c7a19c98b25b744b4bd6ae9e
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-11-23 09:34:44 +0100

    jwt-docker guide

---
+++ src/main/java/ppc/jwtdocker/JwtDockerApplication.java
@@ -0,0 +1,13 @@
+package ppc.jwtdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ppc/jwtdocker/JwtDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package ppc.jwtdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
